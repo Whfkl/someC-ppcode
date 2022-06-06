42aa7b2e88716963d8b59574062e855994dd193b
Fk <84614803+Whfkl@users.noreply.github.com>
2022-06-06 20:28:44 +0800

    Update 7-1 对输入的整数排序.cpp

--- 7-1 对输入的整数排序.cpp
+++ 7-1 对输入的整数排序.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 int main()
 {
-	priority_queue<int, vector<int>, greater<int>> q;
-    for(int t;cin>>t;q.push(t));
-	for (;!q.empty();cout << q.top() << endl,q.pop());
+	priority_queue<int, vector<int>, greater<int>> q;//优先队列
+    	for(int t;cin>>t;q.push(t));//输入
+    /*
+    可以写成
+    int t;
+    while(cin>>t)
+        q.push(t);
+    */
+	for (;!q.empty();cout << q.top() << endl,q.pop());//输出
+    /*
+    可以写成
+    while(!q.empty())
+    {
+        cout<<q.top();
+        q.pop();
+    }
+    */
 }
